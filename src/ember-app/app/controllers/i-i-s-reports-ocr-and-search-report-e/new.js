bc86a647cc357486b7ab93c80d011d3f869dcdd8
AntonIV8 <ananin@neoplatform.ru>
2023-08-16 09:43:54 +0500

    Сгенерировано приложение. Добавлен объект Report с атрибутом файл (#1)
    Co-authored-by: AntonIV8 <anton-iv87@yandex.ru>

---
+++ src/ember-app/app/controllers/i-i-s-reports-ocr-and-search-report-e/new.js
@@ -0,0 +1,4 @@
+import IISReportsOcrAndSearchReportEController from '../i-i-s-reports-ocr-and-search-report-e';
+
+let IISReportsOcrAndSearchReportENewController = IISReportsOcrAndSearchReportEController;
+export default IISReportsOcrAndSearchReportENewController;
---
+++ src/ember-app/app/routes/i-i-s-reports-ocr-and-search-report-e/new.js
@@ -0,0 +1,7 @@
+import EditFormNewRoute from 'ember-flexberry/routes/edit-form-new';
+
+export default EditFormNewRoute.extend({
+  modelProjection: 'ReportE',
+  modelName: 'i-i-s-reports-ocr-and-search-report',
+  templateName: 'i-i-s-reports-ocr-and-search-report-e',
+});
