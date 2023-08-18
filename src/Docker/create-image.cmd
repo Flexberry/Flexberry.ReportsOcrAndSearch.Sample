8d2b3fe0a2ee24322273e618f7accb5ca412016b
AntonIV8 <anton-iv87@yandex.ru>
2023-08-18 09:51:17 +0500

    Добавлен докер с Tesseract

--- src/Docker/create-image.cmd
+++ src/Docker/create-image.cmd
@@ -1,3 +1,5 @@
 docker build --no-cache -f SQL\Dockerfile.PostgreSql -t reportsocrandsearch/postgre-sql ../SQL
 
 docker build --no-cache -f Dockerfile -t reportsocrandsearch/app ../..
+
+docker build --no-cache -f OCR\Dockerfile.Tesseract -t reportsocrandsearch/ocr-service-tesseract .
