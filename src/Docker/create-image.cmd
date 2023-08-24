ce592119434d6b67c698c4e3aa3d6b9c67494048
AntonIV8 <anton-iv87@yandex.ru>
2023-08-24 15:21:43 +0500

    Добавлена сборка github actions и issue templates

--- src/Docker/create-image.cmd
+++ src/Docker/create-image.cmd
@@ -2,6 +2,6 @@ docker build --no-cache -f SQL\Dockerfile.PostgreSql -t reportsocrandsearch/post
 
 docker build --no-cache -f Dockerfile -t reportsocrandsearch/app ../..
 
-docker build --no-cache -f OCR\Dockerfile.Tesseract -t reportsocrandsearch/ocr-service-tesseract .
+docker build --no-cache -f OCR\Dockerfile.Tesseract -t reportsocrandsearch/ocr-service-tesseract ..
 
 docker build --no-cache -f ElasticSearch\Dockerfile.ElasticSearch -t reportsocrandsearch/elasticsearch .
