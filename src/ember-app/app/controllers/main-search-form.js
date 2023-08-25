47a2da39d537c169a30879d7cc97e86e47423485
Valentin Churekov <VChurekov@neoplatform.ru>
2023-08-25 09:49:50 +0500

    doc: comment.

--- src/ember-app/app/controllers/main-search-form.js
+++ src/ember-app/app/controllers/main-search-form.js
@@ -1,6 +1,13 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+    /**
+        Текст для поиска.
+
+        @property searchText
+        @type String
+        @default undefined
+    */
     searchText: undefined,
 
     actions: {
